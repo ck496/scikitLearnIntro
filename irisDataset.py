e56c6dd63120f12f4e288330845451159083a083
ck496 <chriskurian24@gmail.com>
2020-10-11 21:51:32 -0700

    Added logic to make predictions and display

--- irisDataset.py
+++ irisDataset.py
@@ -1,7 +1,18 @@
-from sklearn import  datasets
+from sklearn import datasets
 from sklearn import metrics
 from sklearn.tree import DecisionTreeClassifier
 
 # Load iris datasets
 dataset = datasets.load_iris()
-print(dataset)
+
+#fit a CART model to data
+model = DecisionTreeClassifier()
+model.fit(dataset.data, dataset.target)
+
+#make predictions
+expected = dataset.target
+predicted = model.predict(dataset.data)
+
+#Summarize the fit
+print(metrics.classification_report(expected,predicted))
+print(metrics.confusion_matrix(expected,predicted))
